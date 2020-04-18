0b74848fe2ae11fa90dff1123c6235933dcbc096
Lucas Robitaille <lucrobitaille@ymail.com>
2020-04-18 18:49:42 -0500

    removed problem image

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Jumbotron from "../components/Jumbotron/Jumbotron"
 import MaskImg from "../images/masks.jpg"
-import LiamImg from "../images/src/chillin.jpg"
+import LiamImg from "../images/src/liam.jpg"
 
 const IndexPage = () => {
   return (
