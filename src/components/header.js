4807f7dde18c79b2682a615195d686ea9f0502ff
Lucas Robitaille <lucrobitaille@ymail.com>
2020-04-18 16:36:09 -0500

    added padding on mobile

--- src/components/header.js
+++ src/components/header.js
@@ -40,9 +40,13 @@ const HeaderItem = styled.header`
   margin-bottom: 1.45rem;
   display: flex;
   margin: 0 auto;
-  padding: 0px 2rem;
+  padding: 10px 2rem;
   justify-content: space-between;
   z-index: 1;
+  @media (min-width: 750px) {
+    padding: 0px 2rem;
+  }
+
   h1 {
     margin: 0;
     font-size: 32px;
