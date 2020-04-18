e67020ebf7e4db54f31561b37b6595d9c381d981
Lucas Robitaille <lucrobitaille@ymail.com>
2020-04-17 20:30:59 -0500

    created initial components

--- src/components/header.js
+++ src/components/header.js
@@ -1,34 +1,31 @@
 import { Link } from "gatsby"
+import styled from "styled-components"
 import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+  <HeaderItem>
+    <h1>
+      <Link
+        to="/"
+        style={{
+          color: `black`,
+          textDecoration: `none`,
+        }}
+      >
+        {siteTitle}
+      </Link>
+    </h1>
+
+    <NavLinks>
+      <Link to="/">MaskFAQ</Link>
+      <Link to="/">Mask Directory</Link>
+      <Link to="/">Gallery</Link>
+      <Link to="/">Blog</Link>
+      <Link to="/">Contact Us</Link>
+      <button>Donate</button>
+    </NavLinks>
+  </HeaderItem>
 )
 
 Header.propTypes = {
@@ -40,3 +37,47 @@ Header.defaultProps = {
 }
 
 export default Header
+
+const HeaderItem = styled.header`
+  margin-bottom: 1.45rem;
+  display: flex;
+  margin: 0 auto;
+  padding: 2rem;
+  justify-content: space-between;
+  h1 {
+    margin: 0;
+    font-size: 32px;
+  }
+  a {
+    &:hover {
+      color: dimgrey;
+    }
+  }
+`
+const NavLinks = styled.div`
+  display: inline-flex;
+  font-size: 16px;
+  align-items: center;
+
+  a {
+    margin: 5px;
+    color: black;
+    text-decoration: none;
+    &:hover {
+      color: dimgrey;
+    }
+  }
+
+  button {
+    margin-left: 5px;
+    padding: 10px 20px;
+    background: #253551;
+    color: white;
+    border: none;
+
+    &:hover {
+      cursor: pointer;
+      background: #25355195;
+    }
+  }
+`
--- src/components/layout.js
+++ src/components/layout.js
@@ -29,15 +29,14 @@ const Layout = ({ children }) => {
       <div
         style={{
           margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
         }}
       >
         <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+        <footer
+          style={{ padding: "2rem", background: "#303030", color: "white" }}
+        >
+          <p>Austin Mask Exchange © {new Date().getFullYear()}</p>
+          <p>Austin, TX</p>
         </footer>
       </div>
     </>
