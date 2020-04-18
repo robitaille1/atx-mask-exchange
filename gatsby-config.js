bc47e98f76dbdf628059a5e7aaff331026432ef5
Lucas Robitaille <lucrobitaille@ymail.com>
2020-04-17 20:31:33 -0500

    changed SEO items

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Austin Mask Exchange`,
+    description: `Austin Mask Exchange`,
+    author: `null`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
