30b496da235cf6caaed50d8d836a70acc88b616b
Lucas Robitaille <lucrobitaille@ymail.com>
2020-04-19 12:54:46 -0500

    used gatsby image for query of images

--- src/components/Views/Home.js
+++ src/components/Views/Home.js
@@ -1,100 +1,108 @@
 import React from "react"
 import styled from "styled-components"
 import { Link } from "gatsby"
+import Img from "gatsby-image"
 
 import Jumbotron from "../UI/Jumbotron/Jumbotron"
-import MaskImg from "../../images/masks.jpg"
-import LiamImg from "../../images/liam.jpg"
 
-const Home = () => (
-  <>
-    <Jumbotron />
+const Home = props => {
+  return (
+    <>
+      <Jumbotron image={props.images.grafitti.childImageSharp.fluid} />
+      <CdcInfo>
+        <div style={{ margin: "0px" }}>
+          <h2>The CDC recommends that everyone wear cloth masks.</h2>
+        </div>
+        <div style={{ fontSize: `20px`, margin: "0px" }}>
+          <p>
+            There are many websites and organizations funneling masks to
+            healthcare facilities (which is great). Our goal is to get face
+            masks into the hands of the general public as quickly as possible.
+          </p>
+        </div>
+      </CdcInfo>
 
-    <CdcInfo>
-      <div style={{ margin: "0px" }}>
-        <h2>The CDC recommends that everyone wear cloth masks.</h2>
-      </div>
-      <div style={{ fontSize: `20px`, margin: "0px" }}>
-        <p>
-          There are many websites and organizations funneling masks to
-          healthcare facilities (which is great). Our goal is to get face masks
-          into the hands of the general public as quickly as possible.
-        </p>
-      </div>
-    </CdcInfo>
+      <MaskInfo>
+        <ImgDiv>
+          <Img
+            imgStyle={{ height: `80%` }}
+            fluid={props.images.maskImage.childImageSharp.fluid}
+            alt="masks"
+          />
+        </ImgDiv>
+        <Content>
+          <h3>What You Should Know About Face Masks</h3>
+          <p>
+            Layers, fabrics, filters, patterns and comfortability all make a
+            difference. We’ve put together a FAQ to answer all the questions you
+            may have. If your question is not addressed, do not hesitate to
+            contact us.
+          </p>
 
-    <MaskInfo>
-      <ContentImg src={MaskImg} alt="masks" />
-      <Content>
-        <h3>What You Should Know About Face Masks</h3>
-        <p>
-          Layers, fabrics, filters, patterns and comfortability all make a
-          difference. We’ve put together a FAQ to answer all the questions you
-          may have. If your question is not addressed, do not hesitate to
-          contact us.
-        </p>
+          <Link
+            to="/mask-faq"
+            style={{
+              color: `white`,
+              textDecoration: `none`,
+            }}
+          >
+            <button>Learn More</button>
+          </Link>
+        </Content>
+      </MaskInfo>
+      <Where>
+        <Content style={{ marginTop: "20px" }}>
+          <h3>Where Can I Get A Face Mask In Austin?</h3>
+          <p>
+            We are keeping a running list of all the mask creators in the
+            Austin, Texas area. To request a mask, fill out the form on the
+            directory page. The wonderful volunteers try to prioritize based on
+            need and the date the request was received. One of the mask makers
+            will reach out to you as soon as possible.
+          </p>
+          <Link
+            to="/mask-directory"
+            style={{
+              color: `white`,
+              textDecoration: `none`,
+            }}
+          >
+            <button>Request a Mask</button>
+          </Link>
+        </Content>
+        <ImgDiv>
+          <Img
+            imgStyle={{ height: `80%` }}
+            fluid={props.images.liam.childImageSharp.fluid}
+            alt="Liam making masks"
+          />
+        </ImgDiv>
+      </Where>
+      <ReachOut>
+        <div style={{ margin: "0px auto" }}>
+          <h3>Reach out</h3>
+          <p>
+            If you would like masks for you or your family, please go to the
+            Mask Directory page to contact a maker directly. If you would like
+            to be added to the directory, have materials to contribute, or have
+            any other questions - get in touch! We will get back to you as soon
+            as we can.
+          </p>
 
-        <Link
-          to="/mask-faq"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          <button>Learn More</button>
-        </Link>
-      </Content>
-    </MaskInfo>
-    <Where>
-      <Content style={{ marginTop: "20px" }}>
-        <h3>Where Can I Get A Face Mask In Austin?</h3>
-        <p>
-          We are keeping a running list of all the mask creators in the Austin,
-          Texas area. To request a mask, fill out the form on the directory
-          page. The wonderful volunteers try to prioritize based on need and the
-          date the request was received. One of the mask makers will reach out
-          to you as soon as possible.
-        </p>
-        <Link
-          to="/mask-directory"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          <button>Request a Mask</button>
-        </Link>
-      </Content>
-      <ContentImg
-        style={{ marginTop: "30px" }}
-        src={LiamImg}
-        alt="Liam making masks"
-      />
-    </Where>
-    <ReachOut>
-      <div style={{ margin: "0px auto" }}>
-        <h3>Reach out</h3>
-        <p>
-          If you would like masks for you or your family, please go to the Mask
-          Directory page to contact a maker directly. If you would like to be
-          added to the directory, have materials to contribute, or have any
-          other questions - get in touch! We will get back to you as soon as we
-          can.
-        </p>
-
-        <Link
-          to="/contact-us"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          <button>Reach Out</button>
-        </Link>
-      </div>
-    </ReachOut>
-  </>
-)
+          <Link
+            to="/contact-us"
+            style={{
+              color: `white`,
+              textDecoration: `none`,
+            }}
+          >
+            <button>Reach Out</button>
+          </Link>
+        </div>
+      </ReachOut>
+    </>
+  )
+}
 
 export default Home
 
@@ -159,9 +167,9 @@ const Content = styled.div`
   }
 `
 
-const ContentImg = styled.img`
+const ImgDiv = styled.div`
   width: 100%;
-  height: auto;
+  margin-top: 30px;
   @media (min-width: 900px) {
     width: 40%;
   }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,40 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+
 import Layout from "../components/Utility/layout"
 import SEO from "../components/Utility/seo"
 import Home from "../components/Views/Home"
 
 const IndexPage = () => {
+  const query = useStaticQuery(graphql`
+    query {
+      maskImage: file(relativePath: { eq: "masks.jpg" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      liam: file(relativePath: { eq: "liam.jpg" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      grafitti: file(relativePath: { eq: "grafitti.jpeg" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
   return (
     <Layout>
       <SEO title="Home" />
-      <Home />
+      <Home images={query} />
     </Layout>
   )
 }
