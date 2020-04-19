0e3bbb3fa703a55b8485310c51b3f7d1bc35e289
Lucas Robitaille <lucrobitaille@ymail.com>
2020-04-19 12:00:08 -0500

    moved all content from pages to views - cleaned up pages

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from "../components/Utility/layout"
+import SEO from "../components/Utility/seo"
 
 const NotFoundPage = () => (
   <Layout>
--- src/pages/contact-us.js
+++ src/pages/contact-us.js
@@ -1,50 +1,13 @@
 import React from "react"
-import { Link } from "gatsby"
-import styled from "styled-components"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import GrafImg from "../images/graf2-crop.jpeg"
+import Layout from "../components/Utility/layout"
+import SEO from "../components/Utility/seo"
+import ContactUs from "../components/Views/ContactUs"
 
 const Contact = () => (
   <Layout>
     <SEO title="Contact Us" />
-    <BannerImg src={GrafImg} alt="masks" />
-    <Wrapper>
-      <div>
-        <h2>Contact Us</h2>
-        <p>
-          We need all the help we can get! Reach out to us if you are mask
-          maker, have resources, or anything related to the cause! If you are
-          looking for masks for your or your family, please fill out the form on
-          the{" "}
-          <Link style={{ color: "black" }} to="/mask-directory">
-            directory page
-          </Link>{" "}
-          and a mask maker will get in contact with you.
-        </p>
-        <a style={{ color: "black" }} href="mailto:liam@austinmaskexchange.org">
-          liam@austinmaskexchange.org
-        </a>
-      </div>
-    </Wrapper>
+    <ContactUs />
   </Layout>
 )
 
 export default Contact
-
-const Wrapper = styled.section`
-  padding: 2rem;
-  line-height: 2;
-
-  @media (min-width: 700px) {
-    width: 70%;
-    margin: 10px auto;
-  }
-`
-const BannerImg = styled.img`
-  width: 100%;
-  height: 200px;
-  @media (min-width: 700px) {
-    height: 300px;
-  }
-`
