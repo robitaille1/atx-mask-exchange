4d2d6aeb1e8695ef8b44aa565e0bc4e5f5ec3795
Lucas Robitaille <lucrobitaille@ymail.com>
2020-04-21 16:38:56 -0500

    styled 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,12 +5,23 @@ import SEO from "../components/Utility/seo"
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    <Link to="/">
-      <button>Go Home</button>
-    </Link>
+    <div style={{ textAlign: `center`, margin: `5rem` }}>
+      <SEO title="404: Not found" />
+      <h1>NOT FOUND</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <Link to="/">
+        <button
+          style={{
+            color: `white`,
+            background: "#1b3651",
+            border: `none`,
+            padding: `10px 20px`,
+          }}
+        >
+          Go Home
+        </button>
+      </Link>
+    </div>
   </Layout>
 )
 
